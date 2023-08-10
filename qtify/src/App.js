1a9e42511920ec7c6054caaa4ce139575dbe4fbb
ShivamSingh7280 <shivamsingh7280@gmail.com>
2023-08-10 13:01:33 +0530

    setup done successfully

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>QTify is here!!</h1>
+		<div>
+			<h1>QTify is here and I'm gonna make it!!</h1>
 		</div>
 	);
 }
