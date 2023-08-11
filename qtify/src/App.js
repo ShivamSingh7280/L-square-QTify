56b2c0f863270f6bd4094530dd608c2fd824244f
ShivamSingh7280 <shivamsingh7280@gmail.com>
2023-08-11 23:05:03 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
 	return (
 		<>
 			<NavBar />
+			<HeroSection />
 		</>
 	);
 }
