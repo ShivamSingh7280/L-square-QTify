7188a58f3055e19a1e575a6f1355fc5d47a3501f
ShivamSingh7280 <shivamsingh7280@gmail.com>
2023-08-20 21:32:44 +0530

    section Top Albums added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import HeroSection from "./components/HeroSection/HeroSection";
-import Card from "./components/Card/Card";
+import { fetchTopAlbums } from "./api/api";
+import { useState } from "react";
+import Section from "./components/Section/Section";
 
 function App() {
+	const [topAlbumData, setTopAlbumData] = useState([]);
+
+	const generateTopAlbumData = async () => {
+		const data = await fetchTopAlbums();
+		console.log("data", data);
+		setTopAlbumData(data);
+	};
+
+	useEffect(() => {
+		generateTopAlbumData();
+	}, []);
+
 	return (
 		<>
 			<NavBar />
 			<HeroSection />
-			<Card />
+			<Section title="Top Albums" data={topAlbumData} type="album" />
 		</>
 	);
 }
 
 export default App;
+
+// <Grid container spacing={2}>
+// 				{topAlbumData.map((data) => {
+// 					return (
+// 						<Grid item key={data.id}>
+// 							<Card data={data} type="album" />
+// 						</Grid>
+// 					);
+// 				})}
+// 			</Grid>
--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -1,19 +1,45 @@
 import React from "react";
+import { Tooltip, Chip } from "@mui/material";
 import styles from "./Card.module.css";
-import Button from "../Button/Button";
 
-const Card = () => {
-	return (
-		<>
-			<div className={styles.wrapper}>
-				<div className={styles.innerCard}>
-					<img src="" alt="" width="" height="" />
-					<Button btnText="100 Follows" />
-				</div>
-				<p>New Bollywood</p>
-			</div>
-		</>
-	);
+const Card = ({ data, type }) => {
+	// Ensuring that data is defined and contains the expected properties.
+	if (!data || !data.image || !data.follows || !data.title || !data.songs) {
+		return null; // Return null if data is missing or incomplete.
+	}
+
+	const getCard = (type) => {
+		switch (type) {
+			case "album":
+				return (
+					<Tooltip
+						title={`${data.songs.length} songs`}
+						className={styles.tooltip}
+						placement="top"
+						arrow>
+						<div className={styles.wrapper}>
+							<div className={styles.card}>
+								<img src={data.image} alt="album" />
+								<div className={styles.banner}>
+									<Chip
+										label={`${data.follows} Follows`}
+										className={styles.chip}
+										size="small"
+									/>
+								</div>
+							</div>
+							<div className={styles.titleWrapper}>
+								<p>{data.title}</p>
+							</div>
+						</div>
+					</Tooltip>
+				);
+			default:
+				return null; // Return null if type doesn't match "album"
+		}
+	};
+
+	return getCard(type);
 };
 
 export default Card;
